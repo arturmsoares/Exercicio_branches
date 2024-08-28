c708c9562397997771aab6b45981582df9e29e17
arturmsoares <arturms16@gmail.com>
2024-08-27 22:16:10 -0300

    adicionando

--- src/teste.java
+++ src/teste.java
@@ -1,5 +1,5 @@
-public class teste {
-    private int idade;
+public class App {
+    private String nome;
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
